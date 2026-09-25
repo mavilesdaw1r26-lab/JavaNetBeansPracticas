ccd4ab603ecc7533ed60b8c73a5f82e95c94b800
2star444 <alexbarthe444@gmail.com>
2026-09-25 13:19:28 +0200

    Add print statement to ClasificadorNotas class

--- src/RepasoNotas/ClasificadorNotas.java
+++ src/RepasoNotas/ClasificadorNotas.java
@@ -2,7 +2,5 @@
 package RepasoNotas;
 
 public class ClasificadorNotas {
-    
-    
-    
+    System.out.println("SOY MARC, NO PROGRAMO SUFICIENTE, NO SUDO LA CAMISETA, NO SIENTO LOS COLORES, SOLO ME INTERESA COCOGAMBAS Y SUS PIES");
 }
